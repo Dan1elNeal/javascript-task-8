d181016ceb045ef39a893e39a0ee1c0e202fdedb
Dan1elNeal <ishindanil96@gmail.com>
2017-12-09 02:19:30 +0500

    Пробуем

--- client-core.js
+++ client-core.js
@@ -1,11 +1,83 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
+
+const parseArgs = require('minimist');
+const request = require('request');
+const chalk = require('chalk');
 
 function execute() {
-    // Внутри этой функции нужно получить и обработать аргументы командной строки
-    const args = process.argv;
+    let argv = parseArgs(process.argv.slice(2));
+    let command = argv._[0];
 
-    return Promise.resolve('Это строка будет выведена на консоль');
+    if (command === 'list') {
+        return getMessages(argv.from, argv.to);
+    }
+    if (command === 'send') {
+        return postMessage(argv.from, argv.to, argv.text);
+    }
+}
+
+function getMessages(from, to) {
+    const options = {
+        url: 'http://localhost:8080/messages',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        qs: { from, to }
+    };
+
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(getBeautifulMessages(JSON.parse(body)));
+        });
+    });
+}
+
+function postMessage(from, to, text) {
+    const options = {
+        url: 'http://localhost:8080/messages',
+        method: 'POST',
+        json: true,
+        body: { text },
+        qs: { from, to }
+    };
+
+    return new Promise((resolve, reject) => {
+        request(options, (error, response, body) => {
+            if (error) {
+                reject(error);
+            }
+            resolve(getBeautifulMessage(body));
+        });
+    });
+}
+
+function getBeautifulMessages(messages) {
+    let beautifulMessages = [];
+    messages.forEach(message => {
+        let beautifulMessage = getBeautifulMessage(message);
+        beautifulMessages.push(beautifulMessage);
+    });
+
+    return beautifulMessages.join('\n\n');
+}
+
+function getBeautifulMessage(message) {
+    let beautifulMessage = '';
+
+    if (message.from !== undefined) {
+        beautifulMessage += `${chalk.hex('#f00')('FROM')}: ${message.from}\n`;
+    }
+    if (message.to !== undefined) {
+        beautifulMessage += `${chalk.hex('#f00')('TO')}: ${message.to}\n`;
+    }
+    beautifulMessage += `${chalk.hex('#0f0')('TEXT')}: ${message.text}`;
+
+    return beautifulMessage;
 }
--- server-core.js
+++ server-core.js
@@ -1,12 +1,68 @@
 'use strict';
 
 const http = require('http');
+const { parse: parseUrl } = require('url');
+const { parse: parseQuery } = require('querystring');
+
+const MESSAGES = [];
 
 const server = http.createServer();
 
 server.on('request', (req, res) => {
-    // Тут нужно обработать запрос
-    res.end();
+    const query = parseUrl(req.url);
+    if (query.pathname !== '/messages') {
+        res.statusCode = 404;
+        res.end();
+
+        return;
+    }
+    res.setHeader('Content-Type', 'application/json');
+    if (req.method === 'POST') {
+        handlePost(req, res);
+    }
+    if (req.method === 'GET') {
+        handleGet(req, res);
+    }
 });
+
+function handlePost(req, res) {
+    const { query } = parseUrl(req.url);
+    const { from, to } = parseQuery(query);
+
+    let body = [];
+
+    req
+        .on('data', (chunk) => {
+            body.push(chunk);
+        })
+        .on('end', () => {
+            body = body.toString('utf-8');
+            const { text } = JSON.parse(body);
+
+            let message = { text };
+            if (from !== undefined) {
+                message.from = from;
+            }
+            if (to !== undefined) {
+                message.to = to;
+            }
+
+            MESSAGES.push(message);
+
+            res.end(JSON.stringify(message));
+        });
+}
+
+function handleGet(req, res) {
+    const { query } = parseUrl(req.url);
+    const { from, to } = parseQuery(query);
+
+    let suitableMessages = MESSAGES.filter(message => {
+        return (from === undefined || from === message.from) &&
+            (to === undefined || to === message.to);
+    });
+    res.end(JSON.stringify(suitableMessages));
+}
+
 
 module.exports = server;
